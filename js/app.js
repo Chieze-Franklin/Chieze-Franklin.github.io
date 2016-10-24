0f946e06b05b390980599dd21e9cb0068b2b2c36
Franklin Chieze <chieze.franklin@gmail.com>
2016-10-24 09:25:20 +0100

    removed old files

--- js/app.js
+++ js/app.js
@@ -1,43 +1,69 @@
-angular.module('App', ['ionic'])
+// Ionic Starter App
 
-.config(function ($stateProvider, $urlRouterProvider) {
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'App' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'App.controllers' is found in controllers.js
+angular.module('App', ['ionic', 'App.controllers', 'App.directives'/*'App.menu', 'App.home'*/])
 
-  $stateProvider
-    .state('home', {
-      url: '/home',
-      templateUrl: 'views/home/home.html'
-    })
-    .state('reservation', {
-      url: '/reservation',
-      controller: 'ReservationController',
-      templateUrl: 'views/reservation/reservation.html'
-    })
-    .state('weather', {
-      url: '/weather',
-      controller: 'WeatherController',
-      templateUrl: 'views/weather/weather.html'
-    })
-    .state('restaurants', {
-      url: '/restaurants',
-      controller: 'RestaurantsController',
-      templateUrl: 'views/restaurants/restaurants.html'
-    })
-    .state('tour', {
-      url: '/tour',
-      templateUrl: 'views/tour/tour.html'
-    });
-
-  $urlRouterProvider.otherwise('/tour');
-
-})
+.constant('appConstants', (function(){
+  return {
+    'appName': 'Chieze Franklin',
+    'coverPic': 'img/cover.png',
+    'profilePic': 'img/profile.png',
+    'userName': 'Chieze Franklin',
+    'userRole': 'Software Developer'
+  };
+})())
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+
     }
-    if(window.StatusBar) {
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
       StatusBar.styleDefault();
     }
   });
 })
+
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+
+  .state('app', {
+    url: '/app',
+    abstract: true,
+    templateUrl: 'views/menu/menu.html',
+    controller: 'MenuCtrl'
+  })
+
+  .state('app.home', {
+    url: '/home',
+    views: {
+      'menuContent': {
+        templateUrl: 'views/home/home.html',
+        controller: 'HomeCtrl'
+      }
+    }
+  })
+
+  .state('app.projectlists', {
+    url: '/projectlists',
+    views: {
+      'menuContent': {
+        templateUrl: 'views/projectlists/projectlists.html',
+        controller: 'ProjectListsCtrl'
+      }
+    }
+  })
+
+  ;
+
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/app/home');
+});
